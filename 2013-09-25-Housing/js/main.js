6bae7098268460a1bebdd8082a96d546fc1baf7d
Drew Blumberg <alblumberg21@gmail.com>
2013-09-26 15:21:49 -0500

    Added js housing project

--- 2013-09-25-Housing/js/main.js
+++ 2013-09-25-Housing/js/main.js
@@ -1,5 +1,6 @@
 var cost_per_window = 250;
 var cost_per_sqft = 200;
+var cost_per_gallon = 0.25;
 var rooms = [];
 var total_rooms = 0, total_windows = 0, total_sqft = 0;
 
@@ -9,6 +10,10 @@ function item_cost(num_item, cost) {
 
 function room_area(width, length) {
   return width * length;
+}
+
+function cubic_feet_to_gallons(volume) {
+  return volume * 7.48052;
 }
 
 var response = prompt('Enter a room for the house or leave blank to finish:');
@@ -32,12 +37,31 @@ var total_cost_windows = item_cost(total_windows, cost_per_window);
 var total_cost_sqft = item_cost(total_sqft, cost_per_sqft);
 var total_cost = total_cost_sqft + total_cost_windows;
 
+var pool_response = prompt('Do you have a pool? (yes or no)');
+
+if (pool_response.toLowerCase() == 'yes') {
+  var pool = {};
+  pool.diameter = parseFloat(prompt('What is the pool\'s diameter in feet?'));
+  pool.depth = parseFloat(prompt('What is the pool\'s depth in feet?'));
+
+  var pool_area = pool.diameter * pool.diameter / 4 * Math.PI;
+  var pool_cost = cubic_feet_to_gallons(pool_area * pool.depth) * cost_per_gallon;
+}
+
+
+
 console.log('The number of rooms is ' + total_rooms + '.');
 console.log('The number of windows is ' + total_windows + '.');
 console.log('The number of square feet is ' + total_sqft + '.');
 
 console.log('The total cost of the windows is $' + total_cost_windows + '.');
 console.log('The total cost of the rooms is $' + total_cost_sqft + '.');
-console.log('The total cost of the house is $' + total_cost + '.');
+if (pool_response) {
+  console.log('The total cost of the pool is $' + pool_cost + '.');
+  console.log('The total cost of the house is $' + (total_cost + pool_cost) + '.');
+}
+else {
+  console.log('The total cost of the house is $' + total_cost + '.');
+}
 
 
