e04773d8eaa58e403530eefaa6feb1395bc38a41
Drew Blumberg <alblumberg21@gmail.com>
2013-10-21 11:20:26 -0500

    added grunt file for testing

---
+++ 2013-10-17-Calc-Integration-Testing/Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  // Project Configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    qunit: {
+      master: {
+        options: {
+          urls: ["http://localhost:3333/tests/master.html"]
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 3333,
+          hostname: "localhost",
+          base: "public"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-qunit");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+
+  grunt.registerTask("tests", ["connect", "qunit"]);
+  grunt.registerTask("default", ["qunit"]);
+};
